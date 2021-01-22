5d5ccc8c3c66c7b6c9df7a706ceccf24e02c2956
David Rodriguez <davidrodriguezjenkins@gmail.com>
2021-01-22 13:38:15 -0800

    Changed experience to jumbotron, it populates correctly with less code

--- DavidRodJenPortfolio/Controllers/ResumeController.cs
+++ DavidRodJenPortfolio/Controllers/ResumeController.cs
@@ -20,8 +20,10 @@ namespace DavidRodJenPortfolio.Controllers
         {
             List<Resume> FullResume = new List<Resume>()
         {
-            new Resume{Company = Company1, Position = Company2, Description = Company3},
-            new Resume{Company = "CompanyTest2", Position = "PositionTest2", Description = "DescriptionTest2"}
+            new Resume{Company = Company1, Position = Position1, Description = Description1},
+            new Resume{Company = Company2, Position = Position2, Description = Description2},
+            new Resume{Company = Company3, Position = Position3, Description = Description3},
+            new Resume{Company = Company4, Position = Position4, Description = Description4}
         };
 
             return FullResume;
@@ -29,8 +31,8 @@ namespace DavidRodJenPortfolio.Controllers
         }
 
         string Company1 = "DXC Technologies- Redmond Microsoft Campus-Seattle, WA February 2020- Present";
-        string Company2 = "Senior Field Technician IT/Audio Visual";
-        string Company3 = "Self-managing time and equipment under directive of senior regional managers. " +
+        string Position1 = "Senior Field Technician IT/Audio Visual";
+        string Description1 = "     Self-managing time and equipment under directive of senior regional managers. " +
             "Engage in remote and onsite IT solutions, Quick Assist and streamlined " +
             "solutions via accessible assortment of technical libraries of documentation. " +
             "Solve a variety of issues regarding VPN connections and application hang/crash/freeze. " +
@@ -40,15 +42,40 @@ namespace DavidRodJenPortfolio.Controllers
             "standardization with both new and legacy control devices.Remote services through " +
             "Crestron system programming utilizing IP address notation.";
 
-        string Position1 = "";
-        string Position2 = "";
-        string Position3 = "";
-
-        string Description1 = "";
-        string Description2 = "";
-        string Description3 = "";
 
 
+        string Company2 = "Compass Group PLC- Redmond Microsoft Campus-Seattle, WA July 2015- March 2020";
+        string Position2 = "Audio Visual Technician/Preventative Maintenance Tier 1";
+        string Description2 = "     Inspect technologies and electrical equipment to insure first time quality.Daily use of " +
+            "company inventory control programs.Manage personal equipment inventory utilizing basic coding " +
+            "applications for programming behind network services. Provides onsite technical support for the " +
+            "Microsoft Campus ensuring seamless utilization of conference and private spaces.Having a thorough " +
+            "understanding of audio and visual systems.Troubleshoot and problem solve breaks in connection and " +
+            "repair systems; use of network tools to correct connection issues.Connect and network with clients " +
+            "from both national and international sites, assist in client day-to-day functions and provide immediate support when necessary";
+
+
+
+        string Company3 = "The Seattle Gym- Seattle, WA September 2013- June 2015";
+        string Position3 = "Resident Videographer";
+        string Description3 = "     Conduct LiveStream Media Studio capturing real-time footage for exercise video programs. " +
+            "Manage all teams associated with Videography program. " +
+            "Assembled and fixed exercise equipment using various types of hand, power and pneumatic tools." +
+            "Participated in complete studio remodel including replacing flooring and structural repairs. " +
+            "Supported the team by setting up video equipment using power and pneumatic tools. " +
+            "Inspected videos and used live editing software to direct, cut and assemble footage to insure 100% customer satisfaction.";
+
+
+        string Company4 = "The Seattle Gym- Seattle, WA";
+        string Position4 = "General Manager/Billing Coordinator";
+        string Description4 = "     Led a service team of 35 employees.Worked with team members to streamline processes and improvements. " +
+                "Led monthly safety meetings, and daily stand-up meetings. " +
+                "Daily use of Microsoft Excel, Word and Outlook. " +
+                "Ran individual improvement meetings for each team member and verified each employee had appropriate certifications. " +
+                "Monitored, inspected and engaged in over 3200 billing accounts. " +
+                "Boosted team morale by recognizing individuals.";
 
     }
 }
+
+       
